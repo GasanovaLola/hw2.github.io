a9299dcc2bfa6c99e8ce9193d7289ef5f6fa1dae
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2020-09-21 11:57:15 +0300

    commit first

---
+++ main.js
@@ -0,0 +1,73 @@
+//№1
+var x = prompt("Кто пришел ?");
+
+if (x === "Админ") {
+    var pasvord = prompt("Пароль ?");
+    if (pasvord === "Черный Властелин") {
+        console.log("Добро пожаловать");
+    }
+    else if (x === null) {
+        console.log("Вход отменен");
+    }
+    else {
+        console.log("Пароль неверен");
+    }
+}
+else if (x === null) {
+    console.log("Вход отменен");
+}
+else {
+    console.log("Я вас не знаю");
+}
+// -------------------------------------------------
+
+//№2
+var sum = 0;
+var sumch = 0;
+var size = 150;
+
+for (let index = 1; index <= size; index++) {
+    sum += index;
+    if (index % 2 == 0) {
+        sumch += index; 
+    }
+}
+//№2.1
+console.log("Сумма всех чисел " + sum);
+//№2.2
+console.log("Сумма чётных чисел " + sumch);
+
+//№2.3
+var num = +prompt("Введите число");
+console.log(num);
+while (isNaN(num)) {
+    num = prompt("Введите число");
+}
+
+//№2.4
+var f = prompt("Введите факториал n!");
+var t = 1;
+
+for (let index = 1; index <= f; index++) {
+    t *= index;
+}
+
+console.log(t);
+
+
+// №2.5
+var u = prompt("Введите число для проверки");
+var count = 0;
+
+for (let index = 0; index <= u; index++) {
+    if (u % index == 0) {
+        count++;
+    }
+}
+
+if (count <= 2) {
+    console.log("да");
+}
+else {
+    console.log("нет");
+}
